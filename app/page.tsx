dee230e6532baa2eb33809a31a190a0fc7ef1e7f
uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-08-13 11:28:26 +0100

    more visual improvements made

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
     <div className="min-h-screen  overflow-x-hidden bg-white text-black">
       {/* Hero Section */}
       <section
-        className="relative lg:h-[780px] w-full"
+        className="relative lg:h-[560px] w-full"
         style={{
           clipPath:
             "polygon(0 0, 100% 0, 100% 90%, 75% 100%, 50% 90%, 25% 100%, 0 90%)",
@@ -61,15 +61,7 @@ export default function Home() {
         }}
       >
         <div className="relative w-full h-full">
-          <div
-            className="absolute inset-0 z-0"
-            style={{
-              clipPath:
-                "polygon(0 0, 100% 0, 100% 90%, 40.66% 100%, 33.33% 90%, 0 100%)",
-              WebkitClipPath:
-                "polygon(0 0, 80% 0, 100% 90%, 40.66% 100%, 33.33% 90%, 0 100%)",
-            }}
-          >
+          <div>
             {slides.map((slide, index) => (
               <div
                 key={index}
@@ -96,7 +88,7 @@ export default function Home() {
             />
           </div>
 
-          <div className="relative z-20 container mx-auto px-5 md:px-6  lg:px-0 pt-20 lg:pt-48 pb-20 lg:pb-48 flex  h-full">
+          <div className="relative z-20 container mx-auto px-5 md:px-6  lg:px-0 pt-20 lg:pt-32 pb-20 lg:pb-48 flex  h-full">
             <div className="flex flex-col gap-4 w-full max-w-2xl items-start">
               <h1 className="text-4xl lg:text-6xl font-bold leading-tight text-white">
                 Reliable Protection, Exceptional Quality
@@ -200,228 +192,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Product section */}
-
-      <section id="products" className="py-16 bg-gray-200 text-[#121212]">
-        <div className="container mx-auto px-6 lg:px-8">
-          <h2 className="text-xl lg:text-3xl md:text-4xl font-bold leading-tight mb-4 lg:mb-12 text-center text-gray-900">
-            Our Marine Products and Services
-          </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {[
-              {
-                title: "Life Jackets & Work Vests",
-                description:
-                  "High-quality, durable life jackets and work vests designed for safety and comfort in marine environments.",
-                image: "/images/vest.jpg",
-              },
-              {
-                title: "Immersion Suits",
-                description:
-                  "Protective suits to ensure safety in extreme conditions, offering thermal insulation and buoyancy.",
-                image: "/images/dive-suit.jpeg",
-              },
-              {
-                title: "Emergency Food Rations & Water",
-                description:
-                  "Long-shelf-life rations and potable water for emergency preparedness at sea.",
-                image: "/images/ration.jpg",
-              },
-              {
-                title: "Marine Distress Signals",
-                description:
-                  "Reliable pyrotechnics for signaling distress, ensuring visibility in emergency situations.",
-                image: "/images/signal.jpg",
-              },
-              {
-                title: "Safety Lights",
-                description:
-                  "Durable, high-intensity lights for enhanced visibility and safety on vessels.",
-                image: "/images/flash.jpg",
-              },
-              {
-                title: "Inflatable Life Rafts",
-                description:
-                  "High-quality inflatable life rafts for emergency evacuation and survival at sea.",
-                image: "/images/inflatables.jpg",
-              },
-              {
-                title: "MOB, HRU & Line Thrower",
-                description:
-                  "Man Overboard (MOB) devices, Hydrostatic Release Units (HRU), and line throwers for rescue operations.",
-                image: "/images/hydro-device.png",
-              },
-              {
-                title: "Navigational Aids",
-                description:
-                  "Advanced navigational tools to ensure safe and accurate maritime navigation.",
-                image: "/images/navigation.jpg",
-              },
-              {
-                title: "Engines & Electrical Parts",
-                description:
-                  "Reliable engines and electrical components for vessel maintenance and performance.",
-                image: "/images/engine-parts.jpg",
-              },
-              {
-                title: "Lube Oil",
-                description:
-                  "High-performance lubricants to ensure smooth operation of marine engines.",
-                image: "/images/marine-oil.jpg",
-              },
-              {
-                title: "Wire Ropes & Lifting Gears",
-                description:
-                  "Durable wire ropes, anchor chains, and mooring ropes for secure lifting and docking.",
-                image: "/images/lifting-devices.png",
-              },
-              {
-                title: "Liferaft Rental Service",
-                description:
-                  "Flexible liferaft rental options for short- or long-term maritime operations.",
-                image: "/images/boat-rentals.jpg",
-              },
-            ].map((product, index) => (
-              <div
-                key={index}
-                className="bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition duration-300 transform hover:-translate-y-1"
-              >
-                <div className="mb-4">
-                  <Image
-                    width={500}
-                    height={500}
-                    src={product.image}
-                    alt={product.title}
-                    className="w-full h-[140px]  object-conver"
-                  />
-                </div>
-                <h3 className="text-lg font-bold mb-1 text-gray-900">
-                  {product.title}
-                </h3>
-                <p className="text-gray-600 text-sm font-[lato]">
-                  {product.description}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      {/* Services Section */}
-      {/* <section
-        id="services"
-        className="relative pb-10 z-10"
-        style={{
-          clipPath: "polygon(0 0%, 100% 4%, 100% 100%, 0% 100%)",
-          WebkitClipPath: "polygon(0 0%, 100% 4%, 100% 100%, 0% 100%)",
-        }}
-      >
-        <div className="container mx-auto px-6 lg:px-0 pt-30 lg:pt-32 pb-0">
-          <div className="absolute inset-0 z-10 pointer-events-none">
-            <div className="absolute inset-0 bg-gray-200"></div>
-          </div>
-          <div className="relative z-20">
-            <div className="flex flex-col gap-4 mb-6 items-start transition-all duration-500 ease-in-out">
-              <p className="text-sm font-semibold font-[lato] text-[#5200F5]">
-                Superior Protection
-              </p>
-              <h2 className="font-bold text-5xl text-gray-900">
-                Advanced Safety Solutions Provider
-              </h2>
-              <p className="text-base font-[lato] text-[#121212]">
-                Discover cutting-edge fiberglass services and comprehensive
-                safety equipment solutions tailored to meet Lagos&apos; unique
-                industrial needs.
-              </p>
-            </div>
-            <div className="grid grid-cols-1 lg:grid-cols-3 justify-start gap-10">
-              {[
-                {
-                  img: "/images/helment.jpg",
-                  alt: "Safety",
-                  title: "Comprehensive Safety Gear",
-                  desc: "Offering a wide range of personal protective equipment tailored for various industries.",
-                },
-                {
-                  img: "/images/glove.jpg",
-                  alt: "Worker at fibreglass factory rolling solution through fibreglass sheets",
-                  title: "Custom Fiberglass Fabrication",
-                  desc: "Expertly crafted fiberglass solutions designed to meet specific client needs.",
-                },
-                {
-                  img: "/images/headset.jpg",
-                  alt: "Work safety protection equipment. Industrial protective gear on wooden table, red color background.",
-                  title: "On-site Safety Assessments",
-                  desc: "Professional evaluation of workplace safety to identify and mitigate risks.",
-                },
-                {
-                  img: "/images/worker.jpg",
-                  alt: "Fire Fighter Rescue Training Dummy",
-                  title: "Emergency Response Training",
-                  desc: "Hands-on training sessions to prepare teams for emergency situations.",
-                },
-                {
-                  img: "/images/headset.jpg",
-                  alt: "Fire extinguisher and a protective helmet.",
-                  title: "Durable Equipment Supply",
-                  desc: "Providing high-quality, long-lasting safety equipment to ensure maximum protection.",
-                },
-                {
-                  img: "/images/glove-2.jpg",
-                  alt: "Worker Adjusting Machine with Wrench",
-                  title: "Maintenance and Repair Services",
-                  desc: "Regular maintenance and repair of safety gear to ensure optimal performance.",
-                },
-              ].map((service, index) => (
-                <div
-                  key={index}
-                  className="group relative overflow-hidden rounded-sm md:rounded-md lg:rounded-lg transition-all duration-500 ease-in-out"
-                  style={{ flex: "0 0 calc(33.3333% - 26.6667px)" }}
-                >
-                  <div
-                    className={`transition-all duration-500 ease-in-out delay-${
-                      (index + 1) * 100
-                    } opacity-100 translate-y-0 h-full flex flex-col`}
-                  >
-                    <div className="relative aspect-square">
-                      <Image
-                        src={service.img}
-                        alt={service.alt}
-                        title={service.title}
-                        fill
-                        className="object-cover object-center rounded-sm md:rounded-md lg:rounded-lg"
-                        sizes="(max-width: 768px) 100vw, 33vw"
-                      />
-                    </div>
-                    <div className="flex flex-col items-start py-4 text-gray-900">
-                      <h3 className="text-2xl font-semibold mb-1">
-                        {service.title}
-                      </h3>
-                      <p className="text-base font-[lato]">{service.desc}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div></div>
-      </section> */}
-
-      {/* Call to Action */}
-      <section className="py-16 bg-white my-14 text-black text-center">
-        <div className="container mx-auto px-6  lg:px-0">
-          <h2 className="text-2xl md:text-3xl font-bold mb-4">
-            Protect your workforce with Pacific Safety Solutions, Lagos&apos;
-            premier choice for comprehensive safety equipment installation and
-            maintenance. With unparalleled expertise, we ensure your safety
-            systems are always up to standard. Choose us for reliable service
-            and peace of mind in safeguarding your business environment from
-            potential hazards.
-          </h2>
-        </div>
-      </section>
-
       {/* Testimonials */}
       <section id="testimonials" className="relative">
         <div
@@ -443,7 +213,7 @@ export default function Home() {
           </div>
 
           {/* Content */}
-          <div className="relative px-4 lg:px-0 z-10 container mx-auto  pt-30 lg:pt-32 pb-16 lg:pb-32">
+          <div className="relative px-4 lg:px-0 z-10 container mx-auto  pt-30 lg:pt-32 pb-8 lg:pb-10">
             <div className="flex flex-col gap-4 max-w-5xl mb-12 text-left text-white">
               <p className="text-sm font-[lato] font-semibold">
                 Trusted Feedback
@@ -482,9 +252,9 @@ export default function Home() {
               ].map((testimonial, index) => (
                 <div
                   key={index}
-                  className="flex flex-col gap-6 p-8 md:p-12 bg-[#7533f7] bg-opacity-20 rounded-lg font-[lato] text-[#fafafa] max-w-2xl"
+                  className="flex flex-col gap-6 p-4  bg-[#7533f7] bg-opacity-20 rounded-lg font-[lato] text-[#fafafa] max-w-2xl"
                 >
-                  <div className="relative  w-24 h-24 rounded-full overflow-hidden">
+                  <div className="relative  w-18 h-18 rounded-full overflow-hidden">
                     <Image
                       src={testimonial.image}
                       alt={`- ${testimonial.name}`}
@@ -500,89 +270,6 @@ export default function Home() {
               ))}
             </div>
           </div>
-        </div>
-      </section>
-
-      <section
-        className="relative px-6 lg:px-0 pt-20 mb-14 sm:mb-10 bg-gray-200"
-        id="values"
-        style={{
-          clipPath: "polygon(0 0%, 100% 10%, 100% 100%, 0% 100%)",
-          WebkitClipPath: "polygon(0 0%, 100% 10%, 100% 100%, 0% 100%)",
-        }}
-      >
-        <div id="688e568afa86f3be48523168">
-          <div className="absolute inset-0 z-10 pointer-events-none">
-            <div className="absolute inset-0 z-10 bg-[#f5f4f6]" />
-          </div>
-
-          <div className="relative z-10 container mx-auto pt-16 lg:pt-32 pb-16 lg:pb-32">
-            <div>
-              <div className="transition-all ease-in-out duration-500 opacity-100 translate-y-0 flex flex-col gap-4 mb-6 items-start">
-                <p className="w-max max-w-full body-small !font-semibold text-[#0a001f]">
-                  Assured Safety
-                </p>
-                <h2 className="text-3xl lg:text-5xl font-bold block w-full ml-0 mr-auto text-gray-900">
-                  Delivering Trusted Safety Solutions
-                </h2>
-              </div>
-
-              <div className="grid grid-col-1 relative justify-start gap-10">
-                {[
-                  {
-                    title: "Mission Statement",
-                    content:
-                      "At Pacific Safety Solution Limited we are committed to safety and the principles and values guiding it rules in accordance with national and international safety laws and with customer service of the highest standard, services of high quality which are value specific to the needs of our customers, and our team with the knowledge, right tools, and work environment necessary to propel this company to the forefront of marine safety & survival.",
-                  },
-                  {
-                    title: "Vision Statement",
-                    content:
-                      "To become the best leading marine safety and survival provider company where our customers receive the best technical and professional services with the right team, tools and paper work to deliver a top-notch marine service.",
-                  },
-                ].map((card, index) => (
-                  <div
-                    key={index}
-                    className="group w-full relative overflow-hidden rounded-sm md:rounded-md lg:rounded-lg"
-                    style={{ flex: "0 0 calc(33.3333% - 26.6667px)" }}
-                  >
-                    <div
-                      className={`transition-all ease-in-out duration-500 delay-[${
-                        100 + index * 100
-                      }ms] opacity-100 translate-y-0 h-full`}
-                    >
-                      <div className="flex flex-col lg:flex-wrap h-full rounded-sm md:rounded-md lg:rounded-lg">
-                        <div className="flex flex-col lg:flex-wrap items-start py-2 lg:py-6 text-gray-900">
-                          <h2 className="text-xl font-bold  mb-2 ml-0 mr-auto text-current">
-                            {card.title}
-                          </h2>
-                          <div className="text-base text-justify font-[lato] ml-0 mr-auto text-current">
-                            {card.content}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-
-          {/* SVG ClipPath */}
-          <svg
-            className="absolute bottom-0 left-0"
-            width="1449"
-            height="1206"
-            viewBox="0 0 1449 1206"
-          >
-            <clipPath id="688e568afa86f3be48523168-slantLeft">
-              <rect width="1449" height="1118" fill="transparent" />
-              <path
-                d="M1449 0L0 88V0H1449"
-                transform="translate(0, 1118)"
-                fill="transparent"
-              />
-            </clipPath>
-          </svg>
         </div>
       </section>
 
---
+++ app/products/page.tsx
@@ -0,0 +1,120 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="bg-white">
+      <div className="relative">
+        <div className="absolute top-[18%] lg:top[60%] z-[100] w-full flex items-center justify-center text-white">
+          <h1 className="text-7xl">Products</h1>
+        </div>
+        <Image
+          width={1000}
+          height={600}
+          src="/images/boat-men.jpg"
+          alt="boat men"
+          priority
+          className="brightness-50 w-full h-[180px] lg:h-[300px] object-cover"
+        />
+      </div>
+      <main className="mx-auto px-3 lg:px-0 lg:w-[80%]">
+        <div className="grid grid-cols-2 mt-3 mb-6 lg:grid-cols-3 gap-6">
+          {[
+            {
+              title: "Life Jackets & Work Vests",
+              description:
+                "High-quality, durable life jackets and work vests designed for safety and comfort in marine environments.",
+              image: "/images/vest.jpg",
+            },
+            {
+              title: "Immersion Suits",
+              description:
+                "Protective suits to ensure safety in extreme conditions, offering thermal insulation and buoyancy.",
+              image: "/images/dive-suit.jpeg",
+            },
+            {
+              title: "Emergency Food Rations & Water",
+              description:
+                "Long-shelf-life rations and potable water for emergency preparedness at sea.",
+              image: "/images/ration.jpg",
+            },
+            {
+              title: "Marine Distress Signals",
+              description:
+                "Reliable pyrotechnics for signaling distress, ensuring visibility in emergency situations.",
+              image: "/images/signal.jpg",
+            },
+            {
+              title: "Safety Lights",
+              description:
+                "Durable, high-intensity lights for enhanced visibility and safety on vessels.",
+              image: "/images/flash.jpg",
+            },
+            {
+              title: "Inflatable Life Rafts",
+              description:
+                "High-quality inflatable life rafts for emergency evacuation and survival at sea.",
+              image: "/images/inflatables.jpg",
+            },
+            {
+              title: "MOB, HRU & Line Thrower",
+              description:
+                "Man Overboard (MOB) devices, Hydrostatic Release Units (HRU), and line throwers for rescue operations.",
+              image: "/images/hydro-device.png",
+            },
+            {
+              title: "Navigational Aids",
+              description:
+                "Advanced navigational tools to ensure safe and accurate maritime navigation.",
+              image: "/images/navigation.jpg",
+            },
+            {
+              title: "Engines & Electrical Parts",
+              description:
+                "Reliable engines and electrical components for vessel maintenance and performance.",
+              image: "/images/engine-parts.jpg",
+            },
+            {
+              title: "Lube Oil",
+              description:
+                "High-performance lubricants to ensure smooth operation of marine engines.",
+              image: "/images/marine-oil.jpg",
+            },
+            {
+              title: "Wire Ropes & Lifting Gears",
+              description:
+                "Durable wire ropes, anchor chains, and mooring ropes for secure lifting and docking.",
+              image: "/images/lifting-devices.png",
+            },
+            {
+              title: "Liferaft Rental Service",
+              description:
+                "Flexible liferaft rental options for short- or long-term maritime operations.",
+              image: "/images/boat-rentals.jpg",
+            },
+          ].map((product, index) => (
+            <div
+              key={index}
+              className="bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition duration-300 transform hover:-translate-y-1"
+            >
+              <div className="mb-4">
+                <Image
+                  width={500}
+                  height={500}
+                  src={product.image}
+                  alt={product.title}
+                  className="w-full h-[140px]  object-conver"
+                />
+              </div>
+              <h3 className="text-lg font-bold mb-1 text-gray-900">
+                {product.title}
+              </h3>
+              <p className="text-gray-600 text-sm font-[lato]">
+                {product.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
