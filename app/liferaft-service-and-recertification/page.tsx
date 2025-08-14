4be0778fef9ee0e3b44f7f6b393f850847ea730e
uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-08-14 03:58:36 +0100

    added changes from production branch

--- app/liferaft-service-and-recertification/page.tsx
+++ app/liferaft-service-and-recertification/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
         />
       </div>
       <main className="mx-auto px-3 lg:px-0 lg:w-[80%]">
-        <p className="text-base text-neutral-950 text-justify leading-loose mt-10">
+        <p className="text-base text-neutral-950 text-justify leading-relaxed mt-10">
           At Marine Safety Equipment Service and Sales Company Limited we
           operate a Liferaft service station which is certified and fully
           compliant with IMO Resolution A.761(18) as amended by RESOLUTION
